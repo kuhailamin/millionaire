1c16264974a1befd1643f3ec66796cbaffad7c47
kuhailamin <46251759+kuhailamin@users.noreply.github.com>
2021-06-10 09:40:49 +0400

    v1 for who wants to be a millionaire

---
+++ main.py
@@ -0,0 +1,22 @@
+Q1_AMOUNT=100
+Q2_AMOUNT=200
+Q3_AMOUNT=300
+Q4_AMOUNT=500
+Q5_AMOUNT=1000
+Q6_AMOUNT=Q5_AMOUNT*2 #2000
+Q7_AMOUNT=Q6_AMOUNT*2 #4000
+Q8_AMOUNT=Q7_AMOUNT*2 #8000
+Q9_AMOUNT=Q8_AMOUNT*2 #16000
+Q10_AMOUNT=Q9_AMOUNT*2 #32000
+Q11_AMOUNT=Q10_AMOUNT*2 #64000
+Q12_AMOUNT=125000
+Q13_AMOUNT=Q12_AMOUNT*2 #250000
+Q14_AMOUNT=Q13_AMOUNT*2 #500000
+Q15_AMOUNT=Q14_AMOUNT*2
+
+
+print("I'm George Gherdahi. Welcome to Who Wants to Be a Millionare.")
+name=input("Introduce yourself to the audience")
+print("Welcome "+name+" to the show! We are excited to have you!")
+answer=input("So, tell us. What will you do if you win the million dirhams?")
+print("Wow! What a great idea!")
